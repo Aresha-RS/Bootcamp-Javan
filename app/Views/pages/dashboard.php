4d0df9d5ed746953e0141d40e635d69dceda9099
recha <anggrainirecha@gmail.com>
2021-04-23 09:15:45 +0700

    Update dashboard user

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -1,7 +1,9 @@
 <?= $this->extend('layout/template') ?>
 <?= $this->section('content') ?>
 <div class="container">
+    <!-- Bikin halamana disini yaa -->
     <h1 class="mt-5">Students Dashboard Managemant System</h1>
     <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, quis non, quae nulla culpa eaque provident quisquam iste sunt eos ut? Numquam vel facere illum, consequatur repudiandae voluptas ad facilis?</p>
+    <!-- End bikin halaman -->
 </div>
 <?= $this->endSection() ?>
